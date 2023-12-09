c02ddd8b198662ea29496e2e8341486e2c85e9df
ridwansaputra <ridwan.saputra@fast-8.com>
2023-12-09 20:45:48 +0700

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://riidwanadiitya.github.io',
-  base: '/',
   integrations: [
     react({
       experimentalReactChildren: true,
