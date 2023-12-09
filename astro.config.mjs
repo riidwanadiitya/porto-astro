0f4056d6ad31974a9f1d7a4aea68b1e65080c696
ridwansaputra <ridwan.saputra@fast-8.com>
2023-12-09 20:27:44 +0700

    try to deploy to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://riidwanadiitya.github.io',
+  base: '/ridwan-astro',
   integrations: [
     react({
       experimentalReactChildren: true,
